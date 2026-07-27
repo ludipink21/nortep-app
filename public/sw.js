498df3d1f7d185f00589ae49daf466fa13bbc1c4
NorteP Pesquisa <pesquisadecamponortep@gmail.com>
2026-07-26 21:45:21 -0300

    Corrige contraste do ecossistema no tema noturno

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nortep-pesquisa-v34";
+const CACHE = "nortep-pesquisa-v35";
 const CORE = ["/", "/?acesso=pesquisador", "/favicon.svg", "/manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
