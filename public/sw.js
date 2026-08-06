8a0675ca217a07ce8309b2a6549e149494b958dd
LUDIMILA RODRIGUES <ludipink21@gmail.com>
2026-08-06 16:39:06 -0300

    Atualizar cache para vídeo de abertura

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nortep-pesquisa-v52";
+const CACHE = "nortep-pesquisa-v53";
 const CORE = ["/", "/?acesso=pesquisador", "/?acesso=coordenacao", "/?acesso=administracao", "/inteligencia", "/nortep-icon-v1.png", "/manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
