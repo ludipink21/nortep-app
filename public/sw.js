4fd0aa19ddda2b640c72733aebf4613316c5b0bb
LUDIMILA RODRIGUES <ludipink21@gmail.com>
2026-08-22 21:47:21 -0300

    Atualizar cache para apresentação do Pesquisador

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "nortep-pesquisa-v58";
-const CORE = ["/", "/?acesso=pesquisador", "/?acesso=coordenacao", "/?acesso=administracao", "/inteligencia", "/rede/gestao", "/qualidade", "/nortep-icon-v1.png", "/manifest.webmanifest"];
+const CACHE = "nortep-pesquisa-v59";
+const CORE = ["/", "/?acesso=pesquisador", "/?acesso=coordenacao", "/?acesso=administracao", "/inteligencia", "/rede/gestao", "/qualidade", "/perfil-pesquisador", "/nortep-icon-v1.png", "/manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
   caches.keys()
